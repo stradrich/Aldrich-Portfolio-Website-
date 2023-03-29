4782651f89e00c960aa838d23de48ef6f55cd4ea
stradrich <78803960+stradrich@users.noreply.github.com>
2023-03-29 17:00:57 +0800

    Add files via upload

---
+++ Portfolio-Website/main.js
@@ -0,0 +1,7 @@
+// Select the header element
+                
+// Event listener: Mouse move                
+// When user hovers around the top area, show the header                
+                
+// Event listener: Scroll                
+// When user scroll after a certain height, hide the header, else show the header
